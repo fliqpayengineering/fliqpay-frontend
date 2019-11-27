7bd46c3fddf57e9911535f9fd9ebb789943ed89e
Emmanuel Popoola <olaayo10@gmail.com>
2019-11-27 17:17:43 +0100

    added tailwindcss

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "a8b7a8e60b16beb87c4f";
+/******/ 	var hotCurrentHash = "f9db3cdbecd9d28583f6";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
