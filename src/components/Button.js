4f36568ae28f8dd82acb31ebdb01cc39179802ef
Emmanuel Popoola <olaayo10@gmail.com>
2019-12-02 03:43:46 +0100

    created button component

---
+++ src/components/Button.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+export default function Button({
+  type,
+  text,
+  icon,
+  tooltip,
+  className,
+  width = auto,
+  disabled,
+  onClick
+}) {
+  return (
+    <button
+      className={`uppercase bg-secondary hover:bg-blue-900 text-gray-100 py-4 px-5 rounded inline-flex items-center justify-center text-sm font-medium ${className} w-${width}`}
+      title={tooltip}
+      disabled={disabled}
+      onClick={onClick}
+    >
+      {icon}
+      <span>{text}</span>
+    </button>
+  );
+}
